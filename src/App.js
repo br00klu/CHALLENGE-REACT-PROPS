10070657d7079eef614e5a554bc3a65e5ddba9a0
br00klu <brooklu9@gmail.com>
2021-07-15 20:48:51 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
 
+import Description from './Components/DescriptionComponent/DescriptionComponent';
+import NavBar from './Components/NavBarComponent/NavBarComponent';
+import Projects from './Components/ProjectsComponent/ProjectsComponent';
+import Formx from './Components/ContactComponent/ContactComponent';
+import Footer from './Components/FooterComponent/FooterComponent';
+import avatar from '../src/Assets/John-Doe.jpg';
+import img1 from './Assets/proj1.jpg';
+import img2 from './Assets/proj2.jpg';
+import img3 from './Assets/proj3.jpg';
+import img4 from './Assets/proj4.jpg';
+
 function App() {
+  const title='Project ';
+  const description= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus ex natus quis beatae perspiciatis repellat laudantium aliquam.';
+  const link= 'github';
+
+  const projs=[ //could be optimized if we pass title and descr and link as seperate props
+    {image: img1,
+    title,
+    description,
+    link},
+
+    {image: img2,
+    title,
+    description,
+    link},
+
+    {image: img3,
+    title,
+    description,
+    link},
+
+    {image: img4,
+    title,
+    description,
+    link},
+]
+  //console.log(projs);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar Name='John Doe'/>
+      <Description Name='John Doe' Job='make horrible websites' Avatar={avatar}/>
+      <Projects Projs={projs}/>
+      <Formx/>
+      <Footer Name='GoMyCode' Year={2020}/>
     </div>
   );
 }
